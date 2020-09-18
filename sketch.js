2c4e4bc9f4020284425e006dc7ec8da4a03e4c69
sohaliya <parveen@purplereign.in>
2020-09-18 13:45:58 +0530

    test

--- sketch.js
+++ sketch.js
@@ -38,32 +38,27 @@ function setup(){
 
     bird = new Bird(200,50);
 
-    //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    //strokeWeight(4);
+    
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
     log1.display();
-
     box3.display();
     box4.display();
     pig3.display();
     log3.display();
-
     box5.display();
     log4.display();
     log5.display();
-
     bird.display();
     platform.display();
-    //log6.display();
     slingshot.display();    
 }
 
@@ -74,4 +69,9 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+function keyPressed(){
+    if (keyCode===32){
+        slingshot.attach(bird.body);
+    }
 }
